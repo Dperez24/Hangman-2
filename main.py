753540d9802f200700934a091219c24d078816a8
Dperez24 <91672863+Dperez24@users.noreply.github.com>
2021-12-03 14:29:43 +0000

    working game

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
+import random
 """
 This code is for the game hangman, in which someone must guess
 something from a category with a limited amount of guesses.
 """
-#----------------------------------------------------------------------
-def choose_player():
-  print("hi")
 
-  
+
 #----------------------------------------------------------------------
 def choose_word():
   category = input("Choose a category from the following: Movies, Sports, Foods: ")
-  
+    
   if category == "Movies" or category == "movies":
-    import random
-    movie_choice_list = ['cinderella', 'star wars', 'avatar']  
-    computer_movie_choice = random.choice(movie_choice_list)
-    print(computer_movie_choice)
+    movie_choice_list = [ 'starwars', 'joker', 'avatar', 'psycho', 'alien', 'scarface', 'inception', 'parasite', 'whiplash', 'gladiator', 'avengers']  
+    word = random.choice(movie_choice_list)
   elif category == "Sports" or category == "sports":
-    import random
     sports_choice_list = ['basketball', 'football', 'baseball']  
-    computer_sports_choice = random.choice(sports_choice_list)
-    print(computer_sports_choice)
-  if category == "Foods" or category == "foods":
-    import random
+    word = random.choice(sports_choice_list)
+  elif category == "Foods" or category == "foods":
     movie_choice_list = ['pizza', 'corn', 'pasta']  
-    computer_movie_choice = random.choice(movie_choice_list)
-    print(computer_movie_choice)
+    word = random.choice(movie_choice_list)
+ 
 
-
+  return word
 
 #----------------------------------------------------------------------
-def player_guess():
-  print("hi")
+def player_guess(game_word):
+  print("_" * len(game_word))
+  turn = 12
+  guesses = ""
+  
+  while turn > 0: 
+    
+    fails = 0
+    for char in game_word:
+      
+      if char in guesses:
+         print(char)
+      else: 
+        print("_") 
+        fails = fails + 1
+    
+    if fails == 0:
+      print("winner")
+      break
+      
+    guess = input("Choose a letter: ")
+    guesses += guess 
+    if guess not in game_word:
+      turn = turn - 1
+      print("Wrong")
+      print("turns left: " + str(turn))
+        
+      if turn == 0:
+        print("you lose")
+        break
+  
+    
 
-choose_word()
+
+game_word = choose_word()
+player_guess(game_word)
