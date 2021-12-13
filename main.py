ae08745d535db8bcd27be70a6813dad7e10ef69a
marceloprugue <91672865+marceloprugue@users.noreply.github.com>
2021-12-13 13:39:15 +0000

    glitch hunting

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ something from a category with a limited amount of guesses.
 #----------------------------------------------------------------------
 def choose_word():
   category = input("Choose a category from the following: Movies, Sports, Foods: ")
-  while category != "movies" and category != "sports" and category != "foods" and category != "Movies" and category != "Sports" and category != "Foods":
-    print("Not an input.")
-    category = input("\nWhat would you like to do?: ")
-
-    
+  
+  while category != "movies" and category != "Movies"  and category!= "Sports" and category != "sports" and category != "Foods" and category != "foods":
+    print("Not an input, try again.")
+    category = input("\nChoose a category from the following: Movies, Sports, Foods: ")
+  
   if category == "Movies" or category == "movies":
     movie_choice = [ 'rocky', 'joker', 'avatar', 'psycho', 'alien', 'scarface', 'inception', 'parasite', 'whiplash', 'gladiator', 'avengers', 'batman', 'hamlet', 'jaws', 'aladdin', 'matrix', 'venom', 'baywatch', 'cars', 'terminator', 'titanic', 'godfather']  
     game_word = random.choice(movie_choice)
@@ -23,24 +23,26 @@ def choose_word():
   elif category == "Sports" or category == "sports":
     sports_choice = ['basketball', 'football', 'baseball', 'hockey', 'soccer', 'cricket', 'tennis', 'volleyball', 'rugby', 'golf', 'swimming', 'bowling', 'boxing', 'archery', 'curling']  
     game_word = random.choice(sports_choice)
-    print(len(game_word) + "letters in the word-")
+    print(str(len(game_word)) + " letters in the word")
   elif category == "Foods" or category == "foods":
     foods_choice = ['pizza', 'corn', 'pasta', 'potato', 'rice', 'avocado', 'pancake', 'bread', 'broccoli', 'cheese', 'burrito', 'cereal', 'carrot', 'onion', 'apple', 'banana', 'sandwich', 'steak', 'burger', 'oatmeal', 'strawberry']  
     game_word = random.choice(foods_choice)
- 
-    print(str(len(game_word)) + " letters in the word-")
+    print(str(len(game_word)) + " letters in the word")
+  
   return game_word
-
 #----------------------------------------------------------------------
 def player_guess(game_word):
-  turn = 1
+
+  turn = int(input("\nHow many turns would you like?: "))
+
   guesses = ""
   wrong_guess = ""
   while turn > 0: 
     
     fails = 0
+    
     for char in game_word:
-      
+
       if char in guesses:
          print(char, end="")
          
@@ -49,7 +51,7 @@ def player_guess(game_word):
         fails = fails + 1
     
     if fails == 0:
-      print("\n\nCONGRATULATIONS, YOU WIN!")
+      print("\n\n\033[1;32;40mCONGRATULATIONS, YOU WIN!")
       break
       
     guess = input(" Choose a letter: ")
@@ -66,6 +68,32 @@ def player_guess(game_word):
       if turn == 0:
         print("\nYOU LOSE")
         break
+  
+  single_again = input("\n\033[0;37;40mWould you like to play again?: ")
+  
+  if single_again == "Yes" or single_again == "yes":
+    replit.clear()
+    game_type = menu()
+    while game_type != 1 and game_type != 2 and game_type != 3:
+      print("Not an input.")
+    try:
+      game_type = int(input("\nWhat would you like to do?: "))
+    except:
+      print("\nIncorrect; Please select the game type.")
+
+    if game_type == 1:
+  
+      game_word = choose_word()
+      player_guess(game_word)
+
+    elif game_type == 2: 
+      player()
+  
+  elif single_again == "No" or single_again == "no":
+    print("\nNo problem, thanks for playing!")
+
+    
+
 
 #------------------------------------------------------------------------------------
 def menu():
@@ -73,7 +101,7 @@ def menu():
   tprint("HANGMAN", "Boxing")
   print("Welcome to Hangman! \n" )
   print("_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._")
-  print("\n\nMenu-Options: \n\n [1]Single Player  [2]Multiplayer  [3]Versus AI ")
+  print("\n\nMenu-Options: \n\n [1]Single Player  [2]Multiplayer  ")
   try:
     game_type = int(input("\nWhat would you like to do?: "))
   except:
@@ -83,11 +111,15 @@ def menu():
   replit.clear()
   return game_type
 #--------------------------------------------------------------------------------------
-def player(allturn):
+def player():
   print("Welcome to multiplayer!")
   print("\nIn this mode, 2 players will go head to head in a 1v1 hang man battle.\n Player 1 will go first.")
     
   category = input("\nChoose a category from the following: Movies, Sports, Foods: ")
+
+  while category != "movies" and category != "Movies"  and category!= "Sports" and category != "sports" and category != "Foods" and category != "foods":
+    print("Not an input, try again.")
+    category = input("\nChoose a category from the following: Movies, Sports, Foods: ")
       
   if category == "Movies" or category == "movies":
     movie_choice = [ 'rocky', 'joker', 'avatar', 'psycho', 'alien', 'scarface','inception', 'parasite', 'whiplash', 'gladiator', 'avengers', 'batman', 'hamlet', 'jaws', 'aladdin', 'matrix', 'venom', 'baywatch', 'cars', 'terminator', 'titanic', 'godfather']  
@@ -100,7 +132,7 @@ def player(allturn):
     game_word = random.choice(foods_choice)
   player_1 = True
   while True:  
-    if player_1:
+    if player_1 == True :
       player_1_turn = 10
       guesses = ""
       wrong_guess = ""
@@ -136,6 +168,7 @@ def player(allturn):
             break
       print("Player 1 Used " + str(10 - player_1_turn) + " Turns")
       replit.clear()
+      print("Player 2 Turn.")
       player_1 = False
     else: 
       player_2_turn = 10
@@ -170,19 +203,51 @@ def player(allturn):
           print()
           if player_2_turn == 0:
             print("\nYou ran out of turns.")
-            break
-      print("Player 1 Used " + str(10 - player_2_turn) + " Turns")
+      player_1 = False
+      break
+          
+      print("Player 2 Used " + str(10 - player_2_turn) + " Turns")
       
-    break
-  allturns = [player_1_turn, player_2_turn]
-  return allturns
+   
+  
+
+
+  if player_1_turn > player_2_turn:
+    print("\n\033[1;32;40mPlayer 1 wins!")
+
+  elif player_2_turn > player_1_turn:
+    print("\n\033[1;32;40mPlayer 2 wins!")
+
+  elif player_1_turn == player_2_turn :
+    print("\033[1;32;40mIts a tie!")
+  
+  multi_again = input("\n\033[0;37;40mWould you like to play again?: ")
+  
+  if multi_again == "Yes" or multi_again == "yes":
+    replit.clear()
+    game_type = menu()
+    while game_type != 1 and game_type != 2 and game_type != 3:
+      print("Not an input.")
+    try:
+      game_type = int(input("\nWhat would you like to do?: "))
+    except:
+      print("\nIncorrect; Please select the game type.")
+
+    if game_type == 1:
+  
+      game_word = choose_word()
+      player_guess(game_word)
+
+    elif game_type == 2: 
+      player()
+  
+  elif multi_again == "No" or multi_again == "no":
+    print("\nNo problem, thanks for playing!")
+
+
+      
 #------------------------------------------------------------------------------------
-
-
-
-allturn = None
 game_type = menu()
-
 
 while game_type != 1 and game_type != 2 and game_type != 3:
   print("Not an input.")
@@ -192,22 +257,9 @@ while game_type != 1 and game_type != 2 and game_type != 3:
     print("\nIncorrect; Please select the game type.")
 
 if game_type == 1:
+  
   game_word = choose_word()
   player_guess(game_word)
 
 elif game_type == 2: 
-  player(allturn)
-  allturn = player(allturn)
-  player_1_turn = allturn[0]
-  player_2_turn = allturn[1]
-
-  
-
-  if player_1_turn > player_2_turn:
-    print("\033[1;32;40mPlayer 1 wins!")
-
-  elif player_2_turn > player_1_turn:
-    print("\033[1;32;40mPlayer 2 wins!")
-
-  elif player_1_turn == player_2_turn :
-    print("\033[1;32;40mIts a tie!")
+  player()
